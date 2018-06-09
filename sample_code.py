5fa6ee654eb58a1c9d511d5d7aad59a1be074d59
Kelly Lougheed <klougheed@css.edu>
2018-06-09 20:51:37 +0000

    Add info about tutorial

--- sample_code.py
+++ sample_code.py
@@ -10,6 +10,8 @@ NLTK: https://textminingonline.com/dive-into-nltk-part-i-getting-started-with-nl
 Sentiment analysis: https://opensourceforu.com/2016/12/analysing-sentiments-nltk/
 '''
 
+nltk.download('punkt')
+
 # How to store the contents of a .txt URL into a string and then tokenize it for analysis
 url = "https://www.gutenberg.org/files/768/768.txt"
 response = request.urlopen(url)
@@ -18,12 +20,10 @@ tokens = nltk.word_tokenize(raw)
 text = nltk.Text(tokens)
 
 # How to do a concordance of a word (e.g. "moors")
-res = text.concordance("moors")
-print(res)
+text.concordance("moors")
 
 # How to find the common contexts of one or more words
-res = text.common_contexts(["the", "moors"])
-print(res)
+text.common_contexts(["moors"])
 
 # How to find how many times a word occurs in a text
 print(text.count("Heathcliff"))
@@ -31,7 +31,7 @@ print(text.count("Heathcliff"))
 # How to calculate how often a word occurs in a text
 count = text.count("Heathcliff") # How many times the word occurs
 total = len(text) # Total number of words in the text
-freq = count/total # Percentage of the text made up of that word
+percentage = count/total # Percentage of the text made up of that word
 
 # How to analyze sentiments with polarity scores (positive, negative, neutral)
 nltk.download('vader_lexicon')
@@ -41,23 +41,16 @@ scores = sid.polarity_scores(raw)
 print(scores)
 
 # How to open and read a file
-file = open("wh.txt", "r")
-book = file.read()
-
-# How to get the polarity scores of the first 10 sentences in the file
-book = nltk.sent_tokenize(book) # Tokenize the book into sentences
-for i in range(10): # Repeat 10 times
-    sentence_scores = sid.polarity_scores(book[i])
-    print(sentence_scores)
-
-# Open another file
-file2 = open("katebush.txt", "r")
-lyrics = file2.read()
+file1 = open("katebush.txt", "r")
+lyrics = file1.read()
 
 # How to break up the file by newlines (line breaks) and store it as a list instead of a string
 lyrics = lyrics.split("\n")
 
+# To break up file by sentences: lyrics = nltk.sent_tokenize(lyrics)
+
 # How to get the polarity scores of each lyric
 for lyric in lyrics:
     lyric_scores = sid.polarity_scores(lyric)
+    print(lyric)
     print(lyric_scores)
